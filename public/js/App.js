1233ddecc2202fd4b60a22bcab1b407fbd13c876
joyfeel <joybee210@gmail.com>
2016-03-29 18:02:07 +0800

    Finished: basic functionality

---
+++ public/js/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	constructor() {
+		super();
+	}
+  render() {
+      return (
+        <h1> React Working!</h1>
+      )
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -5,10 +5,30 @@ import session from 'koa-session';
 import serve from 'koa-static'
 import convert from 'koa-convert';
 import router from './router';
+import path from 'path'
+
+import webpack from 'webpack'
+import WebpackDevMiddleware from "koa-webpack-dev-middleware"
+import WebpackHotMiddleware from "koa-webpack-hot-middleware"
+
+import webpackConfig from './webpack.config'
 
 const app = new Koa()
 
-app.use(serve(__dirname + 'public'))
+app.use(serve(__dirname + '/public'))
+
+const compiler = webpack(webpackConfig);
+app.use(convert(WebpackDevMiddleware(compiler, {
+  hot: true,
+  publicPath: webpackConfig.output.publicPath,
+  historyApiFallback: true
+})));
+
+app.use(convert(WebpackHotMiddleware(compiler, {
+  log: console.log,
+  path: '/__webpack_hmr',
+  heartbeat: 10 * 1000
+})));
 
 app.use(logger());
 app.use(convert(bodyParser()));
